e50db690098596b1f531243871b2e314ceecafbd
Julia Schroeter <julia.schroeter@sap.com>
2012-11-08 11:06:30 +0100

    render autocompleted features as well

--- Plugins/org.feature.multi.perspective.classification/src/main/model/classification.cs
+++ Plugins/org.feature.multi.perspective.classification/src/main/model/classification.cs
@@ -7,6 +7,7 @@ OPTIONS {
     additionalDependencies = "org.featuremapper.models.feature, org.feature.multi.perspective.model, org.feature.model.utilities";
 	generateCodeFromGeneratorModel = "true";
 	overrideLaunchConfigurationDelegate = "false";
+	overrideManifest = "false";
 	overridePluginXML = "false";
 	overrideBuilder = "false";
 
@@ -25,7 +26,8 @@ RULES {
 
 	// syntax definition for class 'Classification'
 	Classification ::=  ("stage" #1 id['<','>'] #1 "on" "view" #1 viewgroup['"','"'] #1 ":" !0)
-						 classifiedFeatures* !0; 
+						 classifiedFeatures* 
+						 (#3 "autocomplete" #1 "{" !1 autoCompleteFeatures* #3 "}")? !0 ; 
 
 	// syntax definition for class 'ClassifiedFeature'
 	ClassifiedFeature ::= (#3 "feature" #1 feature['"','"'] #1 classified[Unclassified : "", Unbound: "unbound", Dead: "dead", Alive: "alive"]) !0;
